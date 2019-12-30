00a9693f923680e5e9c1650a83354dcf4d772738
Tyler Neely <t@jujit.su>
2019-12-30 13:55:49 +0100

    add sanitizer script

---
+++ sanitizers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eo pipefail
+
+echo "asan"
+cargo clean
+export RUSTFLAGS="-Z sanitizer=address"
+export ASAN_OPTIONS="detect_odr_violation=0"
+cargo +nightly test --target x86_64-unknown-linux-gnu
+unset ASAN_OPTIONS
+
+echo "lsan"
+cargo clean
+export RUSTFLAGS="-Z sanitizer=leak"
+cargo +nightly run --target x86_64-unknown-linux-gnu
+
+echo "tsan"
+cargo clean
+export RUSTFLAGS="-Z sanitizer=thread"
+cargo +nightly run --target x86_64-unknown-linux-gnu
