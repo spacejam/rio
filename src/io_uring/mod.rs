a5ebb851b1d350c3ad180791757ca09977e9b50e
Tyler Neely <t@jujit.su>
2019-12-31 00:33:04 +0100

    type shuffle

--- src/io_uring/mod.rs
+++ src/io_uring/mod.rs
@@ -9,16 +9,52 @@ use std::{
 mod constants;
 mod kernel_types;
 mod syscall;
-mod types;
 
 use kernel_types::{
     io_uring_cqe, io_uring_params, io_uring_sqe,
 };
 
 use constants::*;
-pub use types::{Cq, Sq, Uring};
 
 use syscall::{enter, setup};
+
+use std::sync::atomic::AtomicU32;
+
+#[derive(Debug, Copy, Clone)]
+pub struct Uring {
+    pub sq: Sq,
+    pub cq: Cq,
+    pub flags: libc::c_uint,
+    pub ring_fd: libc::c_int,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Sq {
+    pub khead: *const AtomicU32,
+    pub ktail: *const AtomicU32,
+    pub kring_mask: *const libc::c_uint,
+    pub kring_entries: *const libc::c_uint,
+    pub kflags: *const libc::c_uint,
+    pub kdropped: *const libc::c_uint,
+    pub array: *mut libc::c_uint,
+    pub sqes: *mut io_uring_sqe,
+    pub sqe_head: libc::c_uint,
+    pub sqe_tail: libc::c_uint,
+    pub ring_sz: usize,
+    pub ring_ptr: *const libc::c_void,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Cq {
+    pub khead: *const AtomicU32,
+    pub ktail: *const AtomicU32,
+    pub kring_mask: *const libc::c_uint,
+    pub kring_entries: *const libc::c_uint,
+    pub koverflow: *const AtomicU32,
+    pub cqes: *mut io_uring_cqe,
+    pub ring_sz: usize,
+    pub ring_ptr: *const libc::c_void,
+}
 
 fn uring_mmap(
     size: usize,
