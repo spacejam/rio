a428ec9110acfa3c825b82cf3b9a83e1aee5af8c
Tyler Neely <t@jujit.su>
2020-01-06 16:51:47 +0100

    add a ton of lints

--- src/lib.rs
+++ src/lib.rs
@@ -89,11 +89,88 @@
 //!     Ok(())
 //! }
 //! ```
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/spacejam/sled/master/art/tree_face_anti-transphobia.png"
+)]
+#![cfg_attr(test, deny(warnings))]
+#![deny(
+    missing_docs,
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_idioms,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unused_qualifications
+)]
+#![deny(
+    // over time, consider enabling the following commented-out lints:
+    // clippy::missing_const_for_fn,
+    // clippy::missing_docs_in_private_items,
+    // clippy::module_name_repetitions,
+    // clippy::multiple_crate_versions,
+    // clippy::unimplemented,
+    // clippy::wildcard_enum_match_arm,
+    // clippy::else_if_without_else,
+    // clippy::float_arithmetic,
+    // clippy::indexing_slicing,
+    clippy::cast_lossless,
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_precision_loss,
+    clippy::cast_sign_loss,
+    clippy::checked_conversions,
+    clippy::decimal_literal_representation,
+    clippy::doc_markdown,
+    clippy::empty_enum,
+    clippy::expl_impl_clone_on_copy,
+    clippy::explicit_into_iter_loop,
+    clippy::explicit_iter_loop,
+    clippy::fallible_impl_from,
+    clippy::filter_map,
+    clippy::filter_map_next,
+    clippy::find_map,
+    clippy::get_unwrap,
+    clippy::if_not_else,
+    clippy::inline_always,
+    clippy::invalid_upcast_comparisons,
+    clippy::items_after_statements,
+    clippy::map_flatten,
+    clippy::match_same_arms,
+    clippy::maybe_infinite_iter,
+    clippy::mem_forget,
+    clippy::multiple_inherent_impl,
+    clippy::mut_mut,
+    clippy::needless_borrow,
+    clippy::needless_continue,
+    clippy::needless_pass_by_value,
+    clippy::non_ascii_literal,
+    clippy::option_map_unwrap_or,
+    clippy::option_map_unwrap_or_else,
+    clippy::path_buf_push_overwrite,
+    clippy::print_stdout,
+    clippy::pub_enum_variant_names,
+    clippy::redundant_closure_for_method_calls,
+    clippy::replace_consts,
+    clippy::result_map_unwrap_or_else,
+    clippy::shadow_reuse,
+    clippy::shadow_same,
+    clippy::shadow_unrelated,
+    clippy::single_match_else,
+    clippy::string_add,
+    clippy::string_add_assign,
+    clippy::type_repetition_in_bounds,
+    clippy::unicode_not_nfc,
+    clippy::unseparated_literal_suffix,
+    clippy::used_underscore_binding,
+    clippy::wildcard_dependencies,
+    clippy::wrong_pub_self_convention,
+)]
 
 use std::io;
 
 mod completion;
-mod fastlock;
 
 #[cfg(target_os = "linux")]
 mod io_uring;
