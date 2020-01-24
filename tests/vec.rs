16c176442955472eedbe002469bab51c77942809
Tyler Neely <t@jujit.su>
2020-01-24 14:55:42 +0100

    check-in vec test

---
+++ tests/vec.rs
@@ -0,0 +1,26 @@
+use std::io::prelude::*;
+
+#[test]
+fn test_vec_value() {
+    let mut file = std::fs::OpenOptions::new()
+        .create(true)
+        .read(true)
+        .write(true)
+        .open("data")
+        .unwrap();
+
+    let ring = rio::new().unwrap();
+
+    let buffer: Vec<u8> = b"hello world!".to_vec();
+    ring.write_at(&file, &buffer, 0)
+        .unwrap()
+        .wait()
+        .unwrap();
+
+    let mut contents = vec![];
+    file.read_to_end(&mut contents).unwrap();
+
+    assert_eq!(contents, b"hello world!".to_vec());
+
+    std::fs::remove_file("data").unwrap();
+}
